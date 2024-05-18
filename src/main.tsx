8b300face785264f1f4391afeea2ff9ed069cf02
vipulkr059 <vipulkr059@gmail.com>
2024-05-18 16:23:05 +0530

    style: Dark theme implemented

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import "./index.css";
 import { UrlProvider } from "./context/UrlContext";
 import HomePage from "./pages/HomePage";
 import { CaptionProvider } from "./context/CaptionContext";
+import { ThemeProvider } from "./context/ThemeProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <CaptionProvider>
-      <UrlProvider>
-        <HomePage />
-      </UrlProvider>
-    </CaptionProvider>
+    <ThemeProvider>
+      <CaptionProvider>
+        <UrlProvider>
+          <HomePage />
+        </UrlProvider>
+      </CaptionProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,12 @@
 import CaptionBox from "@/components/CaptionBox";
+import { ToggleTheme } from "@/components/ToggleTheme";
 import UrlInput from "@/components/UrlInput";
 import VideoPlayer from "@/components/VideoPlayer";
 
 export default function HomePage() {
   return (
     <div className="flex flex-col gap-3 m-2">
+      <ToggleTheme />
       <UrlInput />
       <VideoPlayer />
       <CaptionBox />
